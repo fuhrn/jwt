b01fd1482c101585ca63da0d7639246793742945
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-09-18 09:09:16 -0300

    create-react-app

---
+++ frontend_jwt/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">JWT</a>
+      </header>
+      <main>
+        List Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
