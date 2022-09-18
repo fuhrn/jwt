77df2ebfe25c0bb81c30fe197b659de30e14913c
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-09-18 09:59:43 -0300

    list-products

---
+++ frontend_jwt/src/data.js
@@ -0,0 +1,77 @@
+const data = {
+  products: [
+    {
+      name: "Product 1",
+      slug: "product-1",
+      category: "category-1",
+      image: "https://via.placeholder.com/679x829.jpg", // 679px × 829px
+      price: 120,
+      countInStock: 10,
+      brand: "Samsung",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality Product-1",
+    },
+    {
+      name: "Product 2",
+      slug: "product-2",
+      category: "category-1",
+      image: "https://via.placeholder.com/679x829.jpg", // 679px × 829px
+      price: 140,
+      countInStock: 8,
+      brand: "Samsung",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality Product-2",
+    },
+    {
+      name: "Product 3",
+      slug: "product-3",
+      category: "category-2",
+      image: "https://via.placeholder.com/679x829.jpg", // 679px × 829px
+      price: 160,
+      countInStock: 12,
+      brand: "Apple",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality Product-3",
+    },
+    {
+      name: "Product 4",
+      slug: "product-4",
+      category: "category-2",
+      image: "https://via.placeholder.com/679x829.jpg", // 679px × 829px
+      price: 180,
+      countInStock: 18,
+      brand: "Apple",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality Product-4",
+    },
+    {
+      name: "Product 5",
+      slug: "product-5",
+      category: "category-2",
+      image: "https://via.placeholder.com/679x829.jpg", // 679px × 829px
+      price: 180,
+      countInStock: 18,
+      brand: "Apple",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality Product-4",
+    },
+    {
+      name: "Product 6",
+      slug: "product-6",
+      category: "category-3",
+      image: "https://via.placeholder.com/679x829.jpg", // 679px × 829px
+      price: 180,
+      countInStock: 18,
+      brand: "Apple",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality Product-4",
+    },
+  ],
+};
+export default data;
