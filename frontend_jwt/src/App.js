45c56a890fdb017c007335c12d8aee68dd3beb09
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-09-19 13:45:45 -0300

    create-signup-screen

--- frontend_jwt/src/App.js
+++ frontend_jwt/src/App.js
@@ -8,6 +8,7 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import SigninScreen from "./screens/SigninScreen";
+import SignupScreen from "./screens/SignupScreen";
 import { useContext } from "react";
 import { Store } from "./Store";
 import ShippingAddressScreen from "./screens/ShippingAddressScreen";
@@ -76,6 +77,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
               <Route path="/shipping" element={<ShippingAddressScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
