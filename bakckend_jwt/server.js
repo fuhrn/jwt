6b1418bf7ce4c9ff5fa9e99b32b3dde42b8bc960
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-09-19 08:25:24 -0300

    generate-sample-users

--- bakckend_jwt/server.js
+++ bakckend_jwt/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import data from './data.js';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 // permite que las variables de entorno queden disponibles en server.js.
 dotenv.config();
@@ -16,28 +18,8 @@ mongoose
   });
 
 const app = express();
-
-app.get('/api/products', (req, res) => { 
-  res.send(data.products);
-})
-
-app.get("/api/products/slug/:slug", (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
