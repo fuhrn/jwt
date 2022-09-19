9274e45b24069483af80f3f76124057321866fc5
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-09-19 14:42:46 -0300

    create-place-order-screen

--- frontend_jwt/src/App.js
+++ frontend_jwt/src/App.js
@@ -13,6 +13,7 @@ import { useContext } from "react";
 import { Store } from "./Store";
 import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import PaymentMethodScreen from "./screens/PaymentMethodScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -79,6 +80,7 @@ function App() {
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
               <Route path="/signup" element={<SignupScreen />} />
+              <Route path="/placeorder" element={<PlaceOrderScreen />} />
               <Route path="/shipping" element={<ShippingAddressScreen />} />
               <Route path="/payment" element={<PaymentMethodScreen />} />
               <Route path="/" element={<HomeScreen />} />
