c0250bef788fe1c97bb693ebc8ec230d7d61505b
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-09-19 10:09:48 -0300

    implement-signin-api

--- bakckend_jwt/server.js
+++ bakckend_jwt/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoute.js';
 
 // permite que las variables de entorno queden disponibles en server.js.
 dotenv.config();
@@ -18,8 +19,19 @@ mongoose
   });
 
 const app = express();
+
+// form data in post request is converted to json object in req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use("/api/users", userRouter);
+
+// para capturar errores de forma asincrona. Primer argumento es err.
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
